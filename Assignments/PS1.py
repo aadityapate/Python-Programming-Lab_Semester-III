cd9860aa46b662812aff2dd4d94b42b47edc3dcb
Aditya Pate <101105693+aadityapate@users.noreply.github.com>
2024-09-23 15:08:31 +0530

    Create PS1.py

---
+++ Assignments/PS1.py
@@ -0,0 +1,11 @@
+# Programming***Essentials***in...Python
+print("Programming***Essentials***in...Python")
+
+# Nursery rhyme of your choice (with proper indentation and newline)
+print("Mary had a little lamb \nLittle lamb, little lamb \nMary had a little lamb \nIt's fleece was white as snow \nEverywhere that Mary went \nMary went, Mary went \nEverywhere that Mary went \nThe lamb was sure to go \nIt followed her to school one day \nSchool one day, school one day \nIt followed her to school one day \nWhich was againts the rules")
+
+# Assign three string values say “A”, “B”, “C” to the variables and print them as below: HI, A B C
+a="A"
+b="B"
+c="C"
+print("HI,",a+b+c)
